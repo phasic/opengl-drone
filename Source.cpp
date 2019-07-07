27e0ed56ce7f0b7f3e2889d039cab05edb2c1abe
yoran <yoran@cobben.com>
2019-07-08 00:38:02 +0200

    minimal working, no render though

--- Source.cpp
+++ Source.cpp
@@ -11,7 +11,7 @@ LIGHT 3 = DRONE LIGHT, LET IT ROTATE
 */
 void lightInit(void);
 void lightWorld(void);
-void lightDrone(void); 
+void lightDrone(void);
 
 void lightBody(int start);
 void droneMaterial(void);
@@ -24,58 +24,103 @@ void myinit(void)
 	glBlendFunc(GL_ONE, GL_SRC_ALPHA);
 	glClearColor(0.8, 0.8, 0.8, 0.0);
 	glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4,
-		0, 1, 12, 4, &bezierctrlpoints[0][0][0]);
+					0, 1, 12, 4, &bezierctrlpoints[0][0][0]);
 	glEnable(GL_MAP2_VERTEX_3);
 	glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
 	glClearDepth(1.0);
 	glEnable(GL_DEPTH_TEST);
 
-
-	
 	/*cylinders*/
 	cyl = gluNewQuadric();
 	gluQuadricNormals(cyl, GLU_SMOOTH);
 
 	/*LIGHTING*/
 	lightInit();
-	
-
 }
 void keyboard(unsigned char key, int x, int y)
 {
 	switch (key)
 	{
-	case '1': as = !as; break;
-	case '2': wire = !wire; break;
-	case '3':	if (fly)
-					anim = 1;
-				else
-					anim = !anim; break;
-	case '4': fly = !fly; 
-		anim = 1;  break;
+	case '1':
+		as = !as;
+		break;
+	case '2':
+		wire = !wire;
+		break;
+	case '3':
+		if (fly)
+			anim = 1;
+		else
+			anim = !anim;
+		break;
+	case '4':
+		fly = !fly;
+		anim = 1;
+		break;
 
-	case '5': zeroSwitch = !zeroSwitch; lightControl(0); break;
-	case '6': oneSwitch = !oneSwitch; lightControl(0); break;
-	case '7': twoSwitch = !twoSwitch; lightControl(0); break;
-	case '8': droneSwitch = !droneSwitch; lightControl(0); break;
-	case 'r': scale[0] = 1.0, scale[1] = 1.0, scale[2] = 1.0,			/*reset*/
-		oog[0] = 75.0, oog[1] = 75.0, oog[2] = 75.0,
-		camAngleX = -135, camAngleY = -45;	 break;
-	case 'a': camAngleX -= 1;			 break;								/*rotate*/
-	case 'd': camAngleX += 1;		     break;
-	case 's': camAngleY += 1;            break;
-	case 'w': camAngleY -= 1;            break;
-	case 'h': changeHeight = 2;			 break;
-	case 'H': changeHeight = 1;			 break;
-	case 'l': masterSwitch = !masterSwitch; lightControl(1); break;
+	case '5':
+		zeroSwitch = !zeroSwitch;
+		lightControl(0);
+		break;
+	case '6':
+		oneSwitch = !oneSwitch;
+		lightControl(0);
+		break;
+	case '7':
+		twoSwitch = !twoSwitch;
+		lightControl(0);
+		break;
+	case '8':
+		droneSwitch = !droneSwitch;
+		lightControl(0);
+		break;
+	case 'r':
+		scale[0] = 1.0, scale[1] = 1.0, scale[2] = 1.0, /*reset*/
+				oog[0] = 75.0, oog[1] = 75.0, oog[2] = 75.0,
+		camAngleX = -135, camAngleY = -45;
+		break;
+	case 'a':
+		camAngleX -= 1;
+		break; /*rotate*/
+	case 'd':
+		camAngleX += 1;
+		break;
+	case 's':
+		camAngleY += 1;
+		break;
+	case 'w':
+		camAngleY -= 1;
+		break;
+	case 'h':
+		changeHeight = 2;
+		break;
+	case 'H':
+		changeHeight = 1;
+		break;
+	case 'l':
+		masterSwitch = !masterSwitch;
+		lightControl(1);
+		break;
 
-	case 'q': scale[0] *= 1.1; scale[1] *= 1.1; scale[2] *= 1.1; break;	/*scale*/
-	case 'e': scale[0] /= 1.1; scale[1] /= 1.1; scale[2] /= 1.1; break;
-	
-	case 'v': show = !show; break;
-	case 'f': flat = !flat; break;
+	case 'q':
+		scale[0] *= 1.1;
+		scale[1] *= 1.1;
+		scale[2] *= 1.1;
+		break; /*scale*/
+	case 'e':
+		scale[0] /= 1.1;
+		scale[1] /= 1.1;
+		scale[2] /= 1.1;
+		break;
+
+	case 'v':
+		show = !show;
+		break;
+	case 'f':
+		flat = !flat;
+		break;
 	}
-	
+
 	glutPostRedisplay();
 }
 #pragma region DisplayFunction
@@ -112,12 +157,11 @@ void DisplayAs(void)
 		glEnd();
 		glPopMatrix();
 	}
-
 }
 void DrawFloor(void)
 {
 	glPushMatrix();
-	
+
 	glTranslatef(0, -15, 0);
 	glScalef(1000, 1000, 1000);
 	glBegin(GL_QUADS);
@@ -146,27 +190,26 @@ void DrawBeam(void)
 }
 void DrawCylinders(void)
 {
-	
-	glPushMatrix();
-	materialDrone(FRAME);
-	glRotatef(90, 1, 0, 0);
 
-	glTranslatef(WIDTH / 2, 0, -4);
-	gluCylinder(cyl, 1.0, 1.0, 5, 50, 1);
-	gluDisk(cyl, 0, 1, 50, 1);
-	glPopMatrix();
+	// glPushMatrix();
+	// materialDrone(FRAME);
+	// glRotatef(90, 1, 0, 0);
 
-	glPushMatrix();
+	// glTranslatef(WIDTH / 2, 0, -4);
+	// gluCylinder(cyl, 1.0, 1.0, 5, 50, 1);
+	// gluDisk(cyl, 0, 1, 50, 1);
+	// glPopMatrix();
 
-	glRotatef(90, 1, 0, 0);
+	// glPushMatrix();
 
-	glTranslatef(-WIDTH / 2, 0, -4);
-	gluCylinder(cyl, 1.0, 1.0, 5, 50, 1);
-	gluDisk(cyl, 0, 1, 50, 1);
-	glPopMatrix();
-	
+	// glRotatef(90, 1, 0, 0);
+
+	// glTranslatef(-WIDTH / 2, 0, -4);
+	// gluCylinder(cyl, 1.0, 1.0, 5, 50, 1);
+	// gluDisk(cyl, 0, 1, 50, 1);
+	// glPopMatrix();
 }
-void DrawBezier(int hoek)	/*eig NURBS*/
+void DrawBezier(int hoek) /*eig NURBS*/
 {
 	//Voor Bezier
 	glPushMatrix();
@@ -175,29 +218,25 @@ void DrawBezier(int hoek)	/*eig NURBS*/
 	glScalef(2, 2, 2);
 	glRotatef(hoek, 0, 1, 0);
 	glRotatef(animHoek, 0, 1, 0);
-	glEnable(GL_DEPTH_TEST);  //update depth buffer
+	glEnable(GL_DEPTH_TEST); //update depth buffer
 	glMap2f(GL_MAP2_VERTEX_3, 0, 1, 3, 4, 0, 1, 12, 4, &bezierctrlpoints[0][0][0]);
 
-	glEnable(GL_MAP2_VERTEX_3); 
-	glEnable(GL_AUTO_NORMAL); 
-
+	glEnable(GL_MAP2_VERTEX_3);
+	glEnable(GL_AUTO_NORMAL);
 
 	glMapGrid2f(20, 0.0, 1.0, 20, 0.0, 1.0);
-
 
 	if (wire)
 		glEvalMesh2(GL_LINE, 0, 20, 0, 20);
 	else
-		glEvalMesh2(GL_FILL, 0, 20, 0, 20);  
+		glEvalMesh2(GL_FILL, 0, 20, 0, 20);
 	glPopMatrix();
-
 }
 void DrawPropeller(void)
 {
-	
+
 	glPushMatrix();
-	
-	
+
 	glTranslatef(WIDTH / 2, 3, 0);
 	for (int i = 0; i <= 240; i += 120)
 		DrawBezier(i);
@@ -207,15 +246,13 @@ void DrawPropeller(void)
 	for (int i = 0; i <= 240; i += 120)
 		DrawBezier(i);
 
-	
 	glPopMatrix();
-
 }
 void DrawSkeleton(void)
 {
 	glPushMatrix();
 	DrawBeam();
-	
+
 	DrawCylinders();
 	//materialDrone(PROPELLER);
 	DrawPropeller();
@@ -243,65 +280,56 @@ void DrawTorus(void)
 }
 void DrawCone(void)
 {
+	// glPushMatrix();
+	// materialDrone(FRAME);
+	// glRotatef(90, 1, 0, 0);
+	// glTranslatef(0, 0, -0.5);
 
-	glPushMatrix();
-	materialDrone(FRAME);
-	glRotatef(90, 1, 0, 0);
-	glTranslatef(0, 0, -0.5);
-
-	gluCylinder(cyl, 5.0, 7.5, 5, 100, 100);
-	glPopMatrix();
-
-
-
+	// gluCylinder(cyl, 5.0, 7.5, 5, 100, 100);
+	// glPopMatrix();
 }
 void DrawNurbs(void)
 {
-	glPushMatrix();
+	// glPushMatrix();
 
+	// GLfloat knots[8] = {0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0};
 
-	GLfloat knots[8] = { 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 1.0, 1.0 };
+	// //theNurb = gluNewNurbsRenderer();
+	// gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL);
 
-
-	//theNurb = gluNewNurbsRenderer(); 
-	gluNurbsProperty(theNurb, GLU_DISPLAY_MODE, GLU_FILL); 	
-	
-	
-	
-	gluBeginSurface(theNurb);
-	gluNurbsSurface(theNurb, 8, knots, 8, knots, 4 * 3, 3, &splinectrlpoints[0][0][0], 4, 4, GL_MAP2_VERTEX_3);
-	gluEndSurface(theNurb);
-	glPopMatrix();
+	// gluBeginSurface(theNurb);
+	// gluNurbsSurface(theNurb, 8, knots, 8, knots, 4 * 3, 3, &splinectrlpoints[0][0][0], 4, 4, GL_MAP2_VERTEX_3);
+	// gluEndSurface(theNurb);
+	// glPopMatrix();
 }
 void DrawBody(void)
 {
 	lightBody(1);
 	glPushMatrix();
-	
-		glScalef(2.5, 2.2, 2.5);
-		
+
+	glScalef(2.5, 2.2, 2.5);
+
 	DrawNurbs();
 	glScalef(1, 1.5, 1);
 	glRotatef(180, 1, 0, 0);
-	
+
 	DrawNurbs();
 	lightBody(0);
 	glPopMatrix();
-
 }
 void DrawDrone(void)
 {
-	glPushMatrix();	
+	glPushMatrix();
 	//glColor3f(1, 1, 0);
 	//materialDrone(FRAME);
 	DrawSkeleton();
 	//materialDrone(FRAME);
-	DrawTorus();	
+	DrawTorus();
 	//materialDrone(FRAME);
-	DrawCone();	
+	DrawCone();
 	//materialDrone(BODY);
 	DrawBody();
-	glDisable(GL_LIGHTING);  /* om uw assen steeds rood te houden */
+	glDisable(GL_LIGHTING); /* om uw assen steeds rood te houden */
 	glPopMatrix();
 }
 void teken(void)
@@ -310,18 +338,15 @@ void teken(void)
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
-
 	distance = sqrt(pow(oog[0], 2) + pow(oog[1], 2) + pow(oog[2], 2));
 
-	oog[0] = distance * -sinf(camAngleX*(M_PI / 180)) * cosf((camAngleY)*(M_PI / 180));
-	oog[1] = distance * -sinf((camAngleY)*(M_PI / 180));
-	oog[2] = -distance * cosf((camAngleX)*(M_PI / 180)) * cosf((camAngleY)*(M_PI / 180));
-
+	// oog[0] = distance * -sinf(camAngleX * (M_PI / 180)) * cosf((camAngleY) * (M_PI / 180));
+	// oog[1] = distance * -sinf((camAngleY) * (M_PI / 180));
+	// oog[2] = -distance * cosf((camAngleX) * (M_PI / 180)) * cosf((camAngleY) * (M_PI / 180));
 
 	gluLookAt(oog[0], oog[1], oog[2], ref[0], ref[1], ref[2], up[0], up[1], up[2]);
 
 	DisplayAs();
-//	DrawFloor();
 
 	glRotatef(animFlyHoek, 0, 1, 0);
 	glPushMatrix();
@@ -329,21 +354,13 @@ void teken(void)
 	glScalef(scale[0], scale[1], scale[2]);
 	glTranslatef(75, height, 0);
 
-	
-	
 	if (show)
 	{
-		
-		//lightDrone();
 		lightWorld();
 		DrawDrone();
-		
-
-
 	}
-	
-	glPopMatrix();
 
+	glPopMatrix();
 
 	/*CENTER*/
 	glPushMatrix();
@@ -352,9 +369,6 @@ void teken(void)
 	glTranslatef(151, 0, 0);
 	glutSolidCube(1);
 	glPopMatrix();
-
-
-
 
 	glFlush();
 }
@@ -389,15 +403,15 @@ void animPropeller(int delta)
 	{
 
 		animHoek -= anim * PROPELLERSPEED;
-		if (animHoek > 360.0) animHoek -= 360.0;
-		if (animHoek < -360.0) animHoek += 360.0;
+		if (animHoek > 360.0)
+			animHoek -= 360.0;
+		if (animHoek < -360.0)
+			animHoek += 360.0;
 	}
-
 
 	glutSwapBuffers();
 	glutPostRedisplay();
 	glutTimerFunc(tijd, animPropeller, 0);
-
 }
 
 void animFlyHeight(int delta)
@@ -416,17 +430,13 @@ void animFlyHeight(int delta)
 	if (height == maxHeight)
 		heightAchieved = 1;
 
-
 	glutSwapBuffers();
 	glutPostRedisplay();
 	glutTimerFunc(tijd2, animFlyHeight, 0);
-
-
 }
 
 void animChangeHeight(int delta)
 {
-
 
 	if (heightAchieved && changeHeight == 1)
 	{
@@ -445,7 +455,6 @@ void animChangeHeight(int delta)
 	glutSwapBuffers();
 	glutPostRedisplay();
 	glutTimerFunc(tijd, animChangeHeight, 0);
-
 }
 void animFlyRotation(int delta)
 {
@@ -453,8 +462,10 @@ void animFlyRotation(int delta)
 	{
 		animFlyTilt = 10;
 		animFlyHoek -= FLYROTATIONSPEED;
-		if (animFlyHoek > 360.0) animFlyHoek -= 360.0;
-		if (animFlyHoek < -360.0) animFlyHoek += 360.0;
+		if (animFlyHoek > 360.0)
+			animFlyHoek -= 360.0;
+		if (animFlyHoek < -360.0)
+			animFlyHoek += 360.0;
 	}
 	glutSwapBuffers();
 	glutPostRedisplay();
@@ -511,7 +522,6 @@ void lightControl(int controllAll)
 			glDisable(GL_LIGHT1);
 			glDisable(GL_LIGHT2);
 			glDisable(GL_LIGHT3);
-			
 		}
 		if (masterSwitch)
 		{
@@ -520,7 +530,6 @@ void lightControl(int controllAll)
 			glEnable(GL_LIGHT1);
 			glEnable(GL_LIGHT2);
 			glEnable(GL_LIGHT3);
-			
 		}
 	}
 }
@@ -536,23 +545,23 @@ void lightBody(int start)
 		glDisable(GL_BLEND);
 		glEnable(GL_LIGHTING);
 	}
-
 }
 void materialDrone(int object)
 {
-	
+
 	switch (object)
 	{
-	case PROPELLER:	glEnable(GL_COLOR_MATERIAL);
+	case PROPELLER:
+		glEnable(GL_COLOR_MATERIAL);
 		glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
-		glColor4f(0.6706,0.6706,0.6706 , 1);
-
+		glColor4f(0.6706, 0.6706, 0.6706, 1);
 
 		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular);
 		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess);
 		break;
 
-	case FRAME:	glEnable(GL_COLOR_MATERIAL);
+	case FRAME:
+		glEnable(GL_COLOR_MATERIAL);
 		glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 		glColor4f(0.2, 0.2, 0.2, 1.0);
 
@@ -560,10 +569,10 @@ void materialDrone(int object)
 		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess_2);
 		break;
 
-	case BODY:	glEnable(GL_COLOR_MATERIAL);
+	case BODY:
+		glEnable(GL_COLOR_MATERIAL);
 		glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE);
 		glColor4f(0.5, 0.5, 0.5, 1.0);
-	
 
 		glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, mat_specular_2);
 		glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, shininess_2);
@@ -572,7 +581,7 @@ void materialDrone(int object)
 }
 void lightWorld(void)
 {
-	
+
 	glShadeModel(flat ? GL_FLAT : GL_SMOOTH);
 	//lightDrone();
 	glLightfv(GL_LIGHT0, GL_POSITION, lp0);
@@ -581,30 +590,30 @@ void lightWorld(void)
 
 	glEnable(GL_NORMALIZE);
 	glEnable(GL_LIGHTING);
-	
 }
 
-
 #pragma endregion
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 	if (argc > 1)
 	{
 		typ = argv[1][0];
 		if (typ == 'F')
 		{
-			xmin = -1.0; xmax = 2.0; ymin = -1.0; ymax = 1.5;
+			xmin = -1.0;
+			xmax = 2.0;
+			ymin = -1.0;
+			ymax = 1.5;
 		}
 	}
 	else
-		typ = 'p';      /* default symmetrisch perspectief  */
+		typ = 'p'; /* default symmetrisch perspectief  */
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowPosition(50, 100);
 	glutInitWindowSize(500, 500);
 	glutCreateWindow("Drone");
 	// glutKeyboardFunc(keyboard);
-
 
 	// glutReshapeFunc(herschaal);
 	// myinit();
